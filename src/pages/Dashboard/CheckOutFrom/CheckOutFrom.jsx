4abe7a524d11184b8397500960ce10a9097ea794
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-10-19 01:05:20 +0600

    commit

---
+++ src/pages/Dashboard/CheckOutFrom/CheckOutFrom.jsx
@@ -0,0 +1,56 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
+
+const CheckOutFrom = () => {
+    const [error, setError] = useState('');
+    const stripe = useStripe();
+    const elements = useElements();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            return
+        }
+        const card = elements.getElement(CardElement);
+        if (card == null) {
+            return
+        }
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type:'card',
+            card
+        })
+        if(error){
+            console.log('payment error', error);
+            setError(error.message)
+        }else{
+            console.log('paymentMethod', paymentMethod);
+            setError('')
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button className="btn border-none text-sm my-3 bg-amber-500 rounded" type="submit" disabled={!stripe}>
+                Pay
+            </button>
+            <p className="text-sm text-red-600">{error}</p>
+        </form>
+    );
+};
+
+export default CheckOutFrom;
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,21 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/sectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutFrom from "../CheckOutFrom/CheckOutFrom";
 
+// TODO: get publish key
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_KEY);
 const Payment = () => {
     return (
         <div>
             <SectionTitle subTitle={'Payment-Method'}></SectionTitle>
+            <div>
+                <Elements stripe={stripePromise}>
+                    <CheckOutFrom></CheckOutFrom>
+                </Elements>
+            </div>
         </div>
+       
     );
 };
 
