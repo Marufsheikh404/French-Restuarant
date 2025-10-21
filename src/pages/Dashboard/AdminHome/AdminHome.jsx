5705e83513974d0ac7e3e6e5fe5cdce05a6dd4da
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-10-21 21:55:42 +0600

    chart is done

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -3,12 +3,14 @@ import useAuth from "../../../hooks/useAuth";
 import useAxios from "../../../hooks/Axios/useAxios";
 import { FaDollarSign, FaList } from "react-icons/fa";
 import { FaBowlFood, FaUsers } from "react-icons/fa6";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, } from 'recharts';
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
 const AdminHome = () => {
     const { user } = useAuth();
     const axiosSecure = useAxios();
 
-    const { data: stats } = useQuery({
+    const { data: stats = {} } = useQuery({
         queryKey: ['admin-stats'],
         queryFn: async () => {
             const res = await axiosSecure.get('/admin-stats');
@@ -16,13 +18,23 @@ const AdminHome = () => {
         }
     });
 
-    const { data: chart = {} } = useQuery({
+    const { data: chart = [] } = useQuery({
         queryKey: ['order-stats'],
         queryFn: async () => {
             const result = await axiosSecure.get('/order-stats');
             return result.data;
         }
-    })
+    });
+
+    // shape chart related
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2},${y} C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width},${y + height} Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+        return <path d={getPath(Number(x), Number(y), Number(width), Number(height))} fill={fill} stroke="none" />;
+    };
     return (
         <div>
             <h1 className="text-3xl text-black">
@@ -65,6 +77,32 @@ const AdminHome = () => {
                     <div className="stat-title text-black text-xl font-bold">Orders</div>
                     <div className="stat-value">{stats?.orders || 0}</div>
                 </div>
+            </div>
+
+            <div className="w-1/2 mt-6 border-none">
+                <BarChart
+                    style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
+                    responsive
+                    data={chart}
+                    margin={{
+                        top: 20,
+                        right: 0,
+                        left: 0,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="category" />
+                    <YAxis width="auto" />
+                    <Bar dataKey="quantity" fill="#8884d8" shape={TriangleBar} label={{ position: 'top' }}>
+                        {chart.map((_entry, index) => (
+                            <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+                        ))}
+                    </Bar>
+                </BarChart>
+            </div>
+            <div className="w-1/2">
+
             </div>
         </div>
     );
