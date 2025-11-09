e57a69964f0b16659c5e47cb8d514b9b2387cc5c
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-09 08:43:40 +0600

    change

--- src/pages/bistroBoss/Bistroboss.jsx
+++ src/pages/bistroBoss/Bistroboss.jsx
@@ -2,7 +2,7 @@ const Bistroboss = () => {
     return (
         <div className="card w-full h-auto md:h-96 bg-base-100 shadow-sm bg-hero-patten bg-fixed bg-cover">
             <div className="card-body bg-white m-4 md:m-20 p-6 md:p-10 rounded-lg">
-                <h1 className="text-xl md:text-2xl font-medium text-center mb-4">BISTRO BOSS</h1>
+                <h1 className="text-xl md:text-2xl font-medium text-center mb-4">FRENCH Restaurant</h1>
                 <p className="text-center text-sm md:text-base leading-relaxed">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum
                     <br className="hidden md:block" /> deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at,
