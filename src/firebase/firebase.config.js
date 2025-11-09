df10504036fbe7ac510c9183ef0d121f416912f9
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-09 09:27:59 +0600

    firebase key expired

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,16 +5,24 @@ import { initializeApp } from "firebase/app";
 
 import { getAuth } from "firebase/auth"; // 🔹 Auth import
 // Your web app's Firebase configuration
-const firebaseConfig = {
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId:import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId
+// };
+  const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
-  projectId:import.meta.env.VITE_projectId,
+  projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId
-};
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
- export const auth = getAuth(app);
+export const auth = getAuth(app);
